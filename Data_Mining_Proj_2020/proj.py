9c29445313ae9a2cffd32ba92efc88d28fea822c
Lorenzo Amaranto <lorenzo.amaranto1@gmail.com>
2020-05-26 16:03:23 +0200

    Data Preprocessing 3/n and Plots

--- Data_Mining_Proj_2020/proj.py
+++ Data_Mining_Proj_2020/proj.py
@@ -9,56 +9,39 @@ Created on Sun May 24 09:30:27 2020
 #import pandas as pd
 
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 #import the train data and convert into a dataframe
 
-df = pd.read_csv('/Users/lorenzo/data_Mining_Proj_2020/Data_Mining_Proj_2020/train.csv', sep = ',')
+#N.B. in the csv file data are indexed starting from 2, but they actually start from 0
 
-count_row = df.shape[0]
 
-print("Number of rows of the dataset " + str(count_row))
+df = pd.read_csv('/Users/lorenzo/data_Mining_Proj_2020/Data_Mining_Proj_2020/train.csv', sep = ',') 
 
-print("\n")
+count_row = df.shape[0]             #Number of rows of the dataset 
 
 # <---- Preproc ------>
 
 #Step 1) Count missing values for each attribute, and a missing rate
 
-print("Counting missing value for each attribute")
-
-print("\n")
-
-print(df.isnull().sum())
+df.isnull().sum()    #Counting missing value for each attribute
 
 rate_of_mval = (43/5719)*100
 
-print("\n")
-
-print("Percentage of missing value = " + str(rate_of_mval) + "%")
-
-print("\n")
     
 #step 2) How to treat missing values?
 
 # Compute the statistics for each attribute that has missing values, the choice an appropriate strategy.
 
-#Filling the empty values for attribute "sales at w-1". 
 
-print("\n")
+df.drop(df.index[:1], inplace=True)    #Week 0 does not have a -1 week, given the high std I decided to cutoff the row
 
-print("Statistics for attribute week-1: ")
 
-print("\n")
+promo_stats  = df['volume_on_promo w-1'].describe().transpose()
 
-print(df['sales w-1'].describe().transpose())
+POS_exposed_stats = df['POS_exposed w-1'].describe().transpose()
 
-#Week 0 does not have a -1 week, given the high std I decided to cutoff the row
-
-df.drop(df.index[:1], inplace=True)
-
-print(df.head())  #TODO: should work, check it again
-
-#N.B. in the csv file data are indexed starting from 2, but they actually start from 0
 
 
 for i in range (1,5719):
@@ -66,13 +49,40 @@ for i in range (1,5719):
     if pd.isna(df.loc[i , "sales w-1"]) :
         
        df.loc[i , "sales w-1"] =  df.loc[i-1 , "target"]
+    
+    if pd.isna(df.loc[i , "volume_on_promo w-1"]):  #For the other two attribute, I replaced missing values with the mean value for that attribute
         
+        df.loc[i , "volume_on_promo w-1"] =  promo_stats["mean"]
         
+    if pd.isna(df.loc[i , "POS_exposed w-1"]):
         
+        df.loc[i, "POS_exposed w-1"] = POS_exposed_stats["mean"]
         
 
 
-#print(df['volume_on_promo w-1'].describe().transpose())
-#print(df['POS_exposed w-1'].describe().transpose())
+for sku in df.sku.unique():
+    
+    newdf = df[(df.sku == sku)]
+    
+    newdf['week'] = newdf.index
+    
+    newdf.plot(x = 'week' , y='target', kind = 'line')
+    
+    #xcoords = [185, 237]
+
+    #for xc in xcoords:
+
+    #    plt.axvline(x=xc, c = "r")
+    
+    #if newdf.scope == 1:
+    
+    print("Series of the --> " + str(sku) + " sku")
+    
+
+    plt.show()
+
+
+
+
 
    
